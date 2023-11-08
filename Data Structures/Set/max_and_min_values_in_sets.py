bd8a07ebda96d9b9442bae1d749f02588bef2de4
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2023-11-08 17:31:37 +1300

    max and min values in set example

---
+++ Data Structures/Set/max_and_min_values_in_sets.py
@@ -0,0 +1,24 @@
+# Define a set of cat weights
+cat_weights = {8, 10, 6, 12, 7}
+
+# Find maximum and minimum value in the set
+max_weight = max(cat_weights)
+min_weight = min(cat_weights)
+
+# Display the results
+print("Cat Weights Set:", cat_weights)
+print("Maximum Cat Weight:", max_weight)
+print("Minimum Cat Weight:", min_weight)
+
+
+'''
+Output will be: 
+
+Cat Weights Set: {6, 7, 8, 10, 12}
+Maximum Cat Weight: 12
+Minimum Cat Weight: 6
+
+In this set of cat weights, the maximum weight is 12 pounds, and the minimum weight is 6 pounds. This information 
+could be useful, for instance, for a veterinarian to have a range of weights for domestic cats.
+
+'''
