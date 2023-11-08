33b47a9c91f9121892d2a1c5a770be37f0f67940
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2023-11-08 17:10:07 +1300

    refactoring + iterate example

---
+++ Data Structures/Set/iterate_over_set.py
@@ -0,0 +1,20 @@
+# Define a set of cat breeds
+cat_breeds = {"Siamese", "Persian", "Maine Coon", "Ragdoll", "Bengal"}
+
+# Iterate over the set and print each element
+for breed in cat_breeds:
+    print(breed)
+
+'''
+Output should list each of the breeds in the set: 
+
+Persian
+Bengal
+Siamese
+Maine Coon
+Ragdoll
+
+Note: 
+Remember that sets are unordered, so the order in which the items are printed may vary each time the program is run.
+
+'''
