d619ac434869da13254786f2cfc04d4be82a2f48
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2023-11-08 17:34:39 +1300

    find length of set example

---
+++ Data Structures/Set/find_length_in_set.py
@@ -0,0 +1,19 @@
+# Define a set of cat weights
+cat_weights = {8, 10, 6, 12, 7}
+
+# Calculate the length of the set
+length_of_set = len(cat_weights)
+
+# Display the results
+print("Cat Weights Set:", cat_weights)
+print("Length of the Cat Weights Set:", length_of_set)
+
+
+'''
+Output will be: 
+Cat Weights Set: {6, 7, 8, 10, 12}
+Length of the Cat Weights Set: 5
+
+The length of the cat_weights set is 5, meaning there are five unique weights recorded. This could be a list of 
+different weights for a group of cats, for example, in a cat shelter.
+'''
