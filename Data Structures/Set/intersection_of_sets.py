8a8f9b20bd04cd368d2f0a0147e0266671df4d49
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2023-11-08 17:18:10 +1300

    intersection of sets example

---
+++ Data Structures/Set/intersection_of_sets.py
@@ -0,0 +1,10 @@
+# Define two sets of cat breeds
+set1 = {"Siamese", "Persian", "Maine Coon"}
+set2 = {"Maine Coon", "Ragdoll", "Bengal"}
+
+# Create an intersection of the two sets
+intersection = set1.intersection(set2)
+
+# Print the intersection
+print(intersection)  # Output: {'Maine Coon'}
+
